26cfc02bdc014c5982ae76781e887a9017aa1db3
jaonunesbe <juebabbrj@gmail.com>
2022-05-14 21:17:01 -0300

    Working with basic data types

---
+++ Section3_Assignment/story_time.js
@@ -0,0 +1,40 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+let hearty_welcome="Welcome back, sir!";
+let capsule_type="cryogenic";
+let enemy_of_the_campaign="Comet confederacy";
+let United_Nations_planet="Kapar";
+
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+let pod_records=10  ;
+let amount_of_ammunition=3 ;
+let power_type_of_armor= 5;
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+let armory=['medium-range rocket launcher',
+    'thermal ray gun',
+    'concentrated light rifle',
+    'firebomb',
+    'plasma grenade',
+    'tungsten crystal sword'];
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+let expression_of_agreement= true;
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+console.log('Engineer soldier- '+hearty_welcome);
+console.log("Your " + capsule_type + " pod was shut down so you could help us fight the " +
+ enemy_of_the_campaign+", which are the alliance of species from other planets that have banded together to attack"+
+ " human colonies from the " +United_Nations_planet +" system, and now they're trying to take our cruiser. "+"\n");
+console.log("Super soldier Jack- Ahh... ok, but I'll need weapons."+"\n");
+console.log("Narrador- The engineer then rummages through the crates in the room..."+"\n");
+console.log("Engineer soldier- Ah! Yes!Here it is! Choose as many as you can load!"+"\n");
+console.log('Narrator- Jack leaves the chamber a little stunned and then picks up '+ armory[2]+" and "+armory[0]+"."+"\n" );
+console.log('Super soldier Jack- How long has it been since my pod started with me?'+"\n");
+console.log("Engineer soldier- The records say "+pod_records+" years approximately."+"\n");
+console.log("Super soldier Jack- Okay, time to return to active duty and reinforce the defense of our cruiser's crew."+"\n");
+console.log("Engineer soldier- Before, take these " + amount_of_ammunition + ' extra cartridges, it will be useful for your weapons; In addition, also take a series '+power_type_of_armor+' operating module, it will make it possible for you to be invisible for a certain time.'+"\n");
+console.log("Super soldier Jack- Hmmm... That is "+ expression_of_agreement + ", thanks, man."+"\n");
+console.log("Narrator- The super soldier takes his equipment from the engineer, orders them to open the door to the hibernation room and goes to the starcruiser's corridor, where the soldiers are fighting, opening fire on the invaders.");
